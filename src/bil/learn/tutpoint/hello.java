ceb0bdac8a36d3115c376f5077b72715767a7003
BilCode <bilal.ahmed@vectrasoft.net>
2015-08-25 16:45:03 +0500

    Add thread classes in hello.java class with runable and simple thread class example

--- src/bil/learn/tutpoint/hello.java
+++ src/bil/learn/tutpoint/hello.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 /**
  * Servlet implementation class hello
  */
@@ -42,6 +44,26 @@ public class hello extends HttpServlet {
 			//int[] sorted =sortData.Selection();
 			int[] sorted =sortData.MergeSort(data);
 			response.getWriter().println("Sorted data: "+Arrays.toString(sorted));
+		}else if(request.getParameter("thread")!=null&&(request.getParameter("thread").equals("true")))
+		{
+			/**Thread in runable**/
+			Counter counter = new Counter();
+			RunnableDemo R1 = new RunnableDemo( "Thread-1");
+			R1.setCounter(counter);
+			R1.start();
+
+			RunnableDemo R2 = new RunnableDemo( "Thread-2");
+			R2.setCounter(counter);
+			R2.start();
+			
+			/**Thread**/
+			/*PrintDemo PD = new PrintDemo();
+
+			ThreadDemo T1 = new ThreadDemo( "Thread - 1 ", PD );
+			ThreadDemo T2 = new ThreadDemo( "Thread - 2 ", PD );
+
+			T1.start();
+			T2.start();*/
 		}else
 		{
 			Cookie firstName = new Cookie("first_name",
@@ -97,6 +119,123 @@ public class hello extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+	}
+
+}
+
+class PrintDemo {
+	public  void printCount(String threadname){
+		try {
+			for(int i = 50; i > 0; i--) {
+				System.out.println("Counter   ---   "  + i +" Thread name "+threadname);
+			}
+		} catch (Exception e) {
+			System.out.println("Thread  interrupted.");
+		}
+	}
+
+}
+
+class ThreadDemo extends Thread {
+	private Thread t;
+	private String threadName;
+	PrintDemo  PD;
+
+	ThreadDemo( String name,  PrintDemo pd){
+		threadName = name;
+		PD = pd;
+	}
+	
+	public void run() {
+		PD.printCount(threadName);
+		/*synchronized(PD) {
+		        PD.printCount(threadName);
+		     }*/
+		System.out.println("Thread " +  threadName + " exiting.");
+	}
+
+	public void start ()
+	{
+		System.out.println("Starting " +  threadName );
+		if (t == null)
+		{
+			t = new Thread (this, threadName);
+			t.start ();
+		}
+	}
+
+}
+
+class Counter{
+	long count = 0;
+	private String threadName="";
+
+	public String getThreadName() {
+		return threadName;
+	}
+
+	public void setThreadName(String threadName) {
+		this.threadName = threadName;
+	}
+
+	public synchronized void add(long value,String threadname){
+		setThreadName(threadname);
+		for(int a =0;a<50;a++){
+			//this.count += value;
+			System.out.println("count " +  (count++) +" Thread Name "+threadname );
+		}
+	}
+}
+
+class RunnableDemo implements Runnable {
+	private Thread t;
+	private String threadName;
+	int k=0,g=0;
+
+	private Counter counter ;
+
+	public Counter getCounter() {
+		return counter;
+	}
+
+	public void setCounter(Counter counter) {
+		this.counter = counter;
+	}
+
+	RunnableDemo( String name){
+		threadName = name;
+		System.out.println("Creating " +  threadName );
+	}
+
+	public void run() {
+		System.out.println("Running " +  threadName );
+		runinside();
+		System.out.println("Thread " +  threadName + " exiting.");
+	}
+
+	void runinside()
+	{
+		//for(int i = 40; i > 0; i--) {
+		//System.out.println("Thread: " + threadName + ", " + i+ " g= "+g++ );
+		//counter.setThreadName(threadName);
+		counter.add(1,threadName);
+		// Let the thread sleep for a while.
+		/*try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}*/
+		//}
+	}
+
+	public void start ()
+	{
+		System.out.println("Starting " +  threadName );
+		if (t == null)
+		{
+			t = new Thread (this, threadName);
+			t.start ();
+		}
 	}
 
 }
